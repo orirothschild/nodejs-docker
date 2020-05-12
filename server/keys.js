01c73c59e95dac2d1ec3037b3e1d0cae17c3d906
ori rothschild <rotchildori@gmail.com>
2020-05-12 15:22:45 -0400

    changed create table at postgres

--- server/keys.js
+++ server/keys.js
@@ -1,11 +1,12 @@
 // envs are values that we recive from docker compose env veriables
 module.exports = {
-    redisHost: process.env.REDIS_HOST,
-    redisPort: process.env.REDIS_PORT,
-    pgUser: process.env.PGUSER,
-    pgHost: process.env.PGHOST,
-    pgDatabase: process.env.PGDATABASE,
-    pgPassword: process.env.PGPASSWORD,
-    pgPort: process.env.PGPORT
-  };
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+  pgUser: process.env.PGUSER,
+  pgHost: process.env.PGHOST,
+  pgDatabase: process.env.PGDATABASE,
+  pgPassword: process.env.PGPASSWORD,
+  pgPort: process.env.PGPORT
+};
+
   
